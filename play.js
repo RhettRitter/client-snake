02d5f8692e6cc90c9b7f81ef8d1256e5e69006e8
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-13 15:27:39 +0000

    play connection

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-const connect = function () {
-    const conn = net.createConnection({
-        host: '10.0.2.15',
-        port: 50541
-        })
-    conn.on("connect", () => {
-        console.log('you ded cuz you idled')    
-        // code that does something when the connection is first established
-          });
-        
-
-
-    conn.setEncoding("utf-8");
-
-    return conn;
-
-};
+const connect = require('./client');
 
 console.log("connecting...");
 connect()

a0d5faed040805927f9083d79aef88f1a6dabd08
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-13 17:03:09 +0000

    play.js with exit function

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 const connect = require('./client');
-
 console.log("connecting...");
 connect()
+
+const setupInput = function (conn) {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf-8');
+    stdin.resume();
+    stdin.on("data", key => {
+        handleUserInput(key);
+    });
+    return stdin;
+};
+const handleUserInput = (key) => {
+    const stdout = process.stdout;
+    process.stdin.on('data', (key) => {
+        if (key === '\u0003') {
+            stdout.write("bye bye.\n");
+            process.exit();
+        }
+    });
+}
+setupInput();
